788cbe8c4b7c87e19b03690e16f8f59595f8efff
MostafaHassanHelal <50018284+MostafaHassanHelal@users.noreply.github.com>
2020-06-16 23:05:36 +0200

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        Builder::defaultStringLength(999); // Update defaultStringLength
+        Builder::defaultStringLength(191); // Update defaultStringLength
 
     }
 }
